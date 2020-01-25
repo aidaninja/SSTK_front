9aebe954bef9063d34d8cf478739056f56aa5a9e
aidaninja <junceliz@gmail.com>
2020-01-25 23:26:32 +0900

    storybookを追加およびサンプルを作成

---
+++ src/components/atoms/button/index.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import Button from "./ButtonBase";
+import { storiesOf } from "@storybook/react";
+
+storiesOf("atom/button", module).add("ボタン", () => <Button>ボタン</Button>);

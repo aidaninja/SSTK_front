fdf859dfb5510bf9fffa3644c6497a41ccd168b0
aidaninja <junceliz@gmail.com>
2020-01-26 16:38:55 +0900

    インプットコンポーネントを修正

--- src/components/atoms/inputs/InputBase.jsx
+++ src/components/atoms/inputs/InputBase.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import styled, { css } from "styled-components";
 
 const Input = props => {
-    const { type = "text", label, onChange, placeholder } = props;
+    const {
+        type = "text",
+        label,
+        onChange,
+        placeholder,
+        size = "large"
+    } = props;
     return (
         <StyledInputWrapper>
-            <StyledLabel>{label}</StyledLabel>
+            <StyledLabel size={size}>{label}</StyledLabel>
             <StyledInput
                 type={type}
                 onChange={onChange}
                 placeholder={placeholder ? placeholder : null}
+                size={size}
                 {...props}
             />
         </StyledInputWrapper>
@@ -23,6 +30,24 @@ const inputGen = options => {
 export const NormalInput = inputGen({ type: "text" });
 export const PasswordInput = inputGen({ type: "password" });
 
+const sizePicker = props => {
+    switch (props.size) {
+        case "small":
+            return css`
+                font-size: 1.2rem;
+            `;
+        case "medium":
+            return css`
+                font-size: 1.4rem;
+            `;
+        case "large":
+            return css`
+                font-size: 1.6rem;
+            `;
+        default:
+    }
+};
+
 const StyledInputWrapper = styled.div`
     && {
     }
@@ -31,17 +56,20 @@ const StyledInputWrapper = styled.div`
 const StyledLabel = styled.label`
     && {
         display: block;
+        ${props => sizePicker(props)}
     }
 `;
 
 const StyledInput = styled.input`
     && {
         display: block;
+        width: 100%;
         background: #ebebeb;
         border: none;
         padding: 0.5rem;
         border-radius: 0.5rem;
         outline: none;
+        ${props => sizePicker(props)}
     }
 `;
 
--- src/components/atoms/inputs/index.stories.js
+++ src/components/atoms/inputs/index.stories.js
@@ -3,5 +3,9 @@ import { storiesOf } from "@storybook/react";
 import { NormalInput, PasswordInput } from "./InputBase";
 
 storiesOf("atom/input", module)
-    .add("default", () => <NormalInput label="ユーザーID" />)
-    .add("password", () => <PasswordInput label="パスワード" />);
+    .add("default", () => (
+        <NormalInput style={{ maxWidth: "20rem" }} label="ユーザーID" />
+    ))
+    .add("password", () => (
+        <PasswordInput style={{ maxWidth: "20rem" }} label="パスワード" />
+    ));

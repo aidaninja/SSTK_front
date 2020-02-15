91ae4d724d8a989aab5b9b1439046a17508e079b
Jun Aida <junceliz@gmail.com>
2020-02-15 13:43:26 +0900

    atoms Imageにコンポーネントを追加
    Imageコンポーネントに以下のバリエーションを追加
    ・RoundImage - radiusが50%のImage
    ・RoundBorderImage - radiusが50%でborderつきのImage

--- src/components/atoms/Imgs/index.stories.js
+++ src/components/atoms/Imgs/index.stories.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { Image } from ".";
+import { Image, RoundImage, RoundBorderImage } from ".";
 
-storiesOf("atom/Image", module).add("default", () => (
-    <Image src="https://emojipedia.org//static/img/logo/emojipedia-logo-140.0d779a8a903c.png" />
-));
+const imageSource =
+    "https://emojipedia.org//static/img/logo/emojipedia-logo-140.0d779a8a903c.png";
+
+storiesOf("atom/Image", module)
+    .add("default", () => <Image src={imageSource} />)
+    .add("Round", () => <RoundImage src={imageSource} />)
+    .add("Round border", () => <RoundBorderImage src={imageSource} />);

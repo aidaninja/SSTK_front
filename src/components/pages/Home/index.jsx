8696cd2dc030e5c103d1766e1f0361c4da2f1a8c
Jun Aida <junceliz@gmail.com>
2020-03-27 01:15:20 +0900

    投稿一覧ページを作成

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,59 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { map } from "lodash";
 import PageLayout from "components/templates/PageLayout";
 import PageHeader from "components/organisms/PageHeader";
 import PostItemList from "components/organisms/PostItemList";
-
-//TODO(aida) 仮の表示用のため実装が完了時、削除する。
-const mockPostsProps = {
-    postItems: [
-        {
-            title: "Reactのアニメーション分からないよー",
-            tags: [{ name: "React" }, { name: "CSS" }],
-            postedOn: "02/17/2020",
-            user: {
-                name: "Jun Aida",
-                src:
-                    "https://emojipedia.org//static/img/logo/emojipedia-logo-140.0d779a8a903c.png"
-            },
-            status: "🔥",
-            id: 1
-        },
-        {
-            title: "Reduxの使い方がわかりません",
-            tags: [{ name: "React" }],
-            postedOn: "02/17/2020",
-            user: {
-                name: "Yuki Inoue",
-                src:
-                    "https://emojipedia.org//static/img/logo/emojipedia-logo-140.0d779a8a903c.png"
-            },
-            status: "🔥",
-            id: 2
-        },
-        {
-            title: "中央寄せできない",
-            tags: [{ name: "HTML" }, { name: "CSS" }],
-            postedOn: "02/17/2020",
-            user: {
-                name: "Jun Aida",
-                src:
-                    "https://emojipedia.org//static/img/logo/emojipedia-logo-140.0d779a8a903c.png"
-            },
-            status: "😆",
-            id: 3
-        }
-    ]
-};
+import { firestore } from "utils/firebase/firebase.utils";
 
 const Home = props => {
     const { user } = props;
+    const [postItems, updatePostItems] = useState([]);
+
+    useEffect(() => {
+        console.log("top page...");
+        const fetchData = async () => {
+            const postListRef = firestore.collection("posts");
+            const snapShot = await postListRef.get();
+            const fetchedList = map(snapShot.docs, doc => {
+                const id = doc.id;
+                const { title, postedOn, user } = doc.data();
+                return { id, title, postedOn, user };
+            });
+            updatePostItems(fetchedList);
+        };
+        fetchData();
+    }, []);
+
     return (
         <>
             <PageLayout user={user}>
                 <PageHeader>HOME</PageHeader>
                 {/* TODO(aida)リストがない場合は新規投稿を促す表示をする */}
                 {/* TODO(aida)ローディング中はローディング表示 */}
-                <PostItemList {...mockPostsProps} />
+                <PostItemList postItems={postItems} />
             </PageLayout>
         </>
     );
